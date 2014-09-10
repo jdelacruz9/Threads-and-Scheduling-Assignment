ed75cd33e3b90f81ee916f858161a1268f453e39
Julio de la Cruz <jjdl_cn@hotmail.com>
2014-09-10 18:58:42 -0400

    Almost Done, I have to set the sleep and awake mode

---
+++ mobile.py
@@ -0,0 +1,91 @@
+from threading import Thread, BoundedSemaphore
+import time
+import sys
+import socket
+from Queue import Queue
+
+totalMsg = 10 #total of messages that the server will allow
+schedulerQ = Queue() #shared queue 
+table = {} #table to hold the CPU time that used each Mobile
+
+mutex = BoundedSemaphore() #semaphore to avoid race condition when accesing the queue
+
+host = 'localhost'   # Symbolic name meaning all available interfaces
+port = 4017 # Arbitrary non-privileged port
+ 
+# Datagram (udp) socket
+try :
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    print 'Socket created'
+except socket.error, msg :
+    print 'Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+ 
+ 
+# Bind socket to local host and port
+try:
+    s.bind((host, port))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+     
+print 'Socket bind complete'
+
+#function that will use the receiver thread to put a message in the queue
+def receiverWork(msg):
+	global totalMsg
+	global schedulerQ
+	mutex.acquire() #acquire the semaphore to avoid other threads of putting a message in the queue at the same time
+	schedulerQ.put(msg) #putting the message in the shared queue
+	totalMsg-=1
+	mutex.release() #releasing the semaphore to be used by another thread
+
+def producerWork():
+	global schedulerQ
+	mutex.acquire() #acquire the semaphore to avoid other threads of getting a message from the queue at the same time
+	msg = schedulerQ.get() #getting a message from the shared queue
+	mutex.release() #releasing the semaphore to be used by another thread
+	mobileID = msg.split(":")[0] #taking the mobileID from the message 
+	jobTimer = int(msg.split(":")[1]) #taking the time that the job will be using the CPU from the message
+
+	#checking if the mobileID is already in the table
+	if(mobileID in table): #if it's so, then we have to add the new time of the job
+		table[mobileID] = table[mobileID] + jobTimer
+	else: #if it's not, then we have to add the mobileID to the table and the time of the job
+		table[mobileID] = jobTimer
+
+	time.sleep(jobTimer)#the thread will sleep until the job finished using the CPU
+	
+
+ 
+#now keep talking with the client
+while totalMsg:
+    # receive data from client (data, addr)
+    d = s.recvfrom(1024)
+    data = d[0]
+    addr = d[1]
+     
+    if not data: 
+        break
+     
+    receiver = Thread(target=receiverWork, args=(data,)) #Thread that will put the messages in the queue
+    producer = Thread(target=producerWork) #Thread that will get the messages in the queue, one by one, and will execute them
+
+    receiver.start() #starting the receiver thread
+    producer.start() #starting the producer thread
+
+    reply = "The message has been received"
+     
+    s.sendto(reply , addr)
+    print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.strip()
+     
+
+
+#waiting for the threads to finish
+receiver.join() 
+producer.join()
+
+s.close() #closing the socket
+
+for key, value in table.items():
+		print "Mobile %s consumed %s seconds of CPU time"%(key, value)
---
+++ mobileClient.py
@@ -0,0 +1,38 @@
+from random import randint #for random
+import socket   #for sockets
+import sys  #for exit
+ 
+totalJobs = 2 #quantity of jobs that will be executed or sent
+timeLimit = 5 #time limit that the job will be using the CPU
+mobileID = str(sys.argv[1]) 
+jobs = [0]*totalJobs #creating the array to hold the time limit for each job
+
+#host and port of the server
+host = sys.argv[2] 
+port = int(sys.argv[3])
+
+for i in range(totalJobs):
+    jobs[i] = randint(1,timeLimit)
+
+# create dgram udp socket
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error:
+    print 'Failed to create socket'
+    sys.exit()
+
+#sending the messages with the mobileID and the time of each job
+for i in range(totalJobs):
+    msg = mobileID + ":" + str(jobs[i])
+    try:
+
+        s.sendto(msg, (host, port))
+        d = s.recvfrom(port)
+        reply = d[0]
+        addr = d[1]
+
+        print 'Server reply : ' + reply
+
+    except socket.error, msg:
+        print 'Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
